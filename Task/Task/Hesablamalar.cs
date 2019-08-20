2a0f1be0f15e9de5a97a42769ff89b8e2be33552
Sattar Amrahov <sattar@code.local>
2019-08-20 12:54:08 +0400

    Task project added

---
+++ Task/Task/Hesablamalar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Hesablamalar
+    {
+        // Toplama
+        public double ToplamaMetodu(params double[] menimGozelMassivim)
+        {
+            double result = 0;
+
+            foreach (double massiviminElementi in menimGozelMassivim)
+            {
+                result += massiviminElementi;
+            }
+
+
+            return result;
+        }
+
+
+        // Cixma
+        public double CixmaMetodu(params double[] massiv)
+        {
+            double cixmaninCavabi = 0;
+            if (massiv.Length > 0)
+            {
+                cixmaninCavabi = massiv[0];
+            }
+
+            for (int i = 1; i < massiv.Length; i++)
+            {
+                cixmaninCavabi -= massiv[i];
+            }
+
+            return cixmaninCavabi;
+
+        }
+
+
+
+        // Vurma
+        // Bolme
+        // Mod
+        // Kvadrat
+
+    }
+}
---
+++ Task/Task/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hesablamalar h = new Hesablamalar();
+
+            double resToplama = h.ToplamaMetodu(5.5, 15.9, 44.1);
+            Console.WriteLine(resToplama);
+
+            double resCixma = h.CixmaMetodu(50, 15, 5, 20);
+            Console.WriteLine(resCixma);
+
+            Console.ReadLine();
+        }
+    }
+}
